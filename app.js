2c43f2015ca80a64703d56bb786514e2644a7856
zhaojiaying <xczjy200888@163.com>
2020-11-12 11:09:49 +0800

    text和button组件用法

--- app.js
+++ app.js
@@ -1,39 +1,72 @@
-//app.js
+/*** 注册App时一般做什么：
+   * 1.判断小程序的进入场景
+   * 2.监听生命周期
+   * 3.App()实例只有一个，并且是全局共享的单例对象 */
+
+//app.js 调用App({})是生成一个小程序
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+  //小程序生命周期，onLaunch，onShow，onHide，onError
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function (options) {
+    console.log('小程序初始化完成：onLaunch')
+    console.log(options)
+    //异步调用
+    //获取用户信息
+    wx.getUserInfo({
+      //success是数据拿到之后，再进行回调的
+      success: function(res){
+        console.log(res)
       }
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+    setTimeout(function(){
+      //const error = new Error()
+      //throw error
+    },3000)
   },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    console.log('界面显示出来：onShow')
+    console.log(options)
+    switch(options.scene){
+      case 1001:
+        break;
+      case 1005:
+        break;
+      default:
+        console.log('新的小程序进入场景')
+    }
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    console.log('界面被隐藏时会执行：onHide')
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    console.log('小程序初始化完成：onError')
+  },
+
+  /**
+   * 页面不存在的监听函数
+   */
+  onPageNotFound:function(){
+    console.log('页面不存在的监听函数')
+  },
+
   globalData: {
-    userInfo: null
+    globleName: 'Hello World',
+    globleAge: 20
   }
+
 })
+
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,19 @@
 // pages/home/home.js
-Page({
 
+//getApp()获取App()产生的实例对象
+const app = getApp()
+console.log(app)
+console.log('哈哈哈')
+
+//调用Page({})注册一个页面
+/**
+ * 调用Page({})一般做什么？
+ * 1.生命周期函数
+ * 2.初始化一些数据
+ * 3.监听wxml中的事件
+ * 4.其他的一些监听，如页面滚动、上拉刷新、下拉加载
+ */
+Page({
   /**
    * 页面的初始数据
    */
@@ -13,7 +26,8 @@ Page({
       {id:112, name:'Tom', age:10},
       {id:113, name:'Jerry', age:12},
     ],
-    counter:0
+    counter:0,
+    list:[]
   },
 
   add(){
@@ -29,6 +43,70 @@ Page({
     this.setData({
       counter: this.data.counter-1
     })
+  },
+
+  handleGetUserInfo(event){
+    console.log('处理点击获取授权按钮')
+    console.log(event)
+
+  },
+
+  handleViewClick(){
+    console.log('啦啦啦被点击了')
+  },
+
+  //监听页面的滚动
+  onPageScroll(obj){
+    console.log(obj.scrollTop)
+  },
+
+  //监听页面滚动到底部
+  onReachBottom(){
+    console.log('页面滚动到底部')
+  },
+
+  //监听页面下来刷新
+  onPullDownRefresh(){
+    console.log('页面下拉刷新')
+  },
+
+  /**
+   * ----------page的生命周期函数-----------
+   */
+  //页面加载
+  onLoad(){
+    console.log('onLoad')
+    wx.request({
+      url: 'http://www.baidu.com',
+      success:(res)=>{
+        console.log(res)
+        const data = res.errMsg
+        this.setData({
+          list: ['哈哈',res.errMsg]
+        })
+      }
+    })
+  },
+
+  //页面显示
+  onShow(){
+    console.log('onShow')
+  },
+
+  //页面初次渲染完成
+  onReady(){
+    console.log('onReady')
+  },
+
+  //页面隐藏
+  onHide(){
+    console.log('onHide')
+  },
+
+  //页面卸载
+  onUnload(){
+    console.log('onUnload')
   }
+
 
 })
